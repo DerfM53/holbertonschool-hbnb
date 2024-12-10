0ea84c3f78da6932ac00e87af01da4f73f29ad8f
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-12-10 15:27:38 +0100

    trying to make custom error password messages

--- part4/scripts.js
+++ part4/scripts.js
@@ -4,5 +4,36 @@
 */
 
 document.addEventListener('DOMContentLoaded', () => {
-  /* DO SOMETHING */
+  const form = document.getElementById('login-form'); // recup le login
+  const emailField = document.getElementById('email'); //recup l'email
+  const passwordField = document.getElementById('password'); // recup mdp
+
+  if (form) { // Si formulaire existe
+    form.addEventListener('submit', function(event) {
+        let valid = true;
+
+        if (!emailField.value) {
+          emailField.setCustomValidity("Veuillez entrer votre email.");
+          valid = false; //formulaire non soumis
+        } else {
+          emailField.setCustomValidity(""); //reset l'erreur
+        }
+
+        // validation champ mdp
+        if (!passwordField.value) {
+          passwordField.setCustomValidity("Veuillez entrer votre mot de passe")
+          valid = false;
+        } else if (passwordField.value.length < 6) {
+          passwordField.setCustomValidity("Le mot de passe doit contenir au moins 6 caractères. :)");
+          valid = false;
+        } else {
+          passwordField.setCustomValidity(""); // réinitialisation erreur
+        }
+
+         // Empêche la soumission si le formulaire n'est pas valide
+         if (!valid) {
+          event.preventDefault(); // empêche l'envoi du formulaire
+        }
+    });
+  }
 });
